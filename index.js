b9570a52e86967a66e9a98a48ee259b15232f103
sittora <sittora@gmail.com>
2021-08-10 21:30:46 -0400

    commit tree 6b845eb2deac97b9330d1841991d535acfc13b12

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+function multi(a, b) {
+    return  b * a;
+}
+
+const multiply = multi(num1, num2);
+
+
+function rand(){
+    return Math.floor(Math.random() * 10) + 1;
+}
+
+const random = rand();
+
+
+let num3 = 8;
+let num4 = 2;
+
+function divi(num3, num4) {
+    return num3 / num4;
+}
+
+const mod = divi(num3, num4);
+
+
+
+function ma(){
+return Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
+}
+
+const max = ma();
